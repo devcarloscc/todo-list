2a6ee01b8ffb882c056647ce41f4dab890129c5e
Carlos Henrique <carlosccdev@gmail.com>
2025-01-25 17:42:47 -0300

    Crianção de addTodo para adicionar TODOS na lista

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import TodoForm from './TodoForm'
 
-const Todo = ({todo}) => {
+const Todo = ({ todo }) => {
   return (
     <div className="todo" key={todo.id}>
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">
-                ({todo.category})
-              </p>
-            </div>
-            <div>
-              <button type="button">Completar</button>
-              <button type="button">X</button>
-            </div>
-            <TodoForm />
-          </div>
+      <div className="content">
+        <p>{todo.text}</p>
+        <p className="category">
+          ({todo.category})
+        </p>
+      </div>
+      <div>
+        <button className='complete' type="button">Completar</button>
+        <button className='remove ' type="button">X</button>
+      </div>
+    </div>
   )
 }
 
